d40d170ca77ed5ae9802e300d0a6ff9909b5da75
Milad Irannejad <moorara@users.noreply.github.com>
2021-10-12 13:06:48 -0400

    Increate project test coverage

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -1,28 +1,89 @@
 package config
 
 import (
+	"io"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func setupConfigFile(file string) (func(), error) {
+	src, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer src.Close()
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+
+	destPath := filepath.Join(homeDir, configFiles[0])
+
+	dst, err := os.Create(destPath)
+	if err != nil {
+		return nil, err
+	}
+	defer dst.Close()
+
+	if _, err := io.Copy(dst, src); err != nil {
+		return nil, err
+	}
+
+	cleanup := func() {
+		_ = os.Remove(destPath)
+	}
+
+	return cleanup, nil
+}
+
 func TestFromFile(t *testing.T) {
+	t.Run("NoConfigFile", func(t *testing.T) {
+		configFiles = []string{"null"}
+		config, err := FromFile()
+		assert.NoError(t, err)
+		assert.Equal(t, Config{}, config)
+	})
+
 	tests := []struct {
 		name           string
-		configFiles    []string
+		configFile     string
 		expectedConfig Config
 		expectedError  string
 	}{
 		{
-			name:           "NoConfigFile",
-			configFiles:    []string{"test/null"},
+			name:           "EmptyConfigFile",
+			configFile:     "test/empty.yaml",
 			expectedConfig: Config{},
+			expectedError:  "EOF",
+		},
+		{
+			name:           "InvalidConfigFile",
+			configFile:     "test/invalid.yaml",
+			expectedConfig: Config{},
+			expectedError:  "yaml: unmarshal errors",
+		},
+		{
+			name:       "Success",
+			configFile: "test/valid.yaml",
+			expectedConfig: Config{
+				GitHub: GitHub{
+					AccessToken: "ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst",
+				},
+			},
 		},
 	}
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			configFiles = tc.configFiles
+			configFiles = []string{".basil.test.yaml"}
+			cleanup, err := setupConfigFile(tc.configFile)
+			assert.NoError(t, err)
+			defer cleanup()
+
 			config, err := FromFile()
 
 			if tc.expectedError != "" {
